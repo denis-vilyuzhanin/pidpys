af0edee2e80d72a654ad2b6a9faec59eeaa81da1
Denys Viliuzhanin <dvilyuzhanin@gmail,com>
2015-03-03 23:50:54 +0200

    Add Key Info Panel Implements Next / Previous Buttons

--- pidpys-standalone/src/main/java/org/pidpys/standalone/ui/AbstractWizardWindow.java
+++ pidpys-standalone/src/main/java/org/pidpys/standalone/ui/AbstractWizardWindow.java
@@ -6,7 +6,13 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -22,12 +28,17 @@ public abstract class AbstractWizardWindow extends JFrame {
 	private JButton previousButton;
 	private JButton cancelButton;
 	private JPanel bodyPanel;
+	private String currentStep;
+	private Map<String, StepPanel> steps = new HashMap<>();
+	private Deque<String> passedSteps = new LinkedList<>();
 	
 	public AbstractWizardWindow() {
 		build();
 	}
 
 	private void build() {
+		setSize(640, 480);
+		setResizable(false);
 		JPanel windowLayout = new JPanel();
 		windowLayout.setLayout(new BorderLayout());
 		super.add(windowLayout);
@@ -37,11 +48,14 @@ public abstract class AbstractWizardWindow extends JFrame {
 		Border border = BorderFactory.createEtchedBorder();
 		buttonsPanel.setBorder(BorderFactory.createEtchedBorder());
 		
-		nextButton = new JButton("Next");
-		buttonsPanel.add(nextButton);
-		
 		previousButton = new JButton("Previous");
+		previousButton.addActionListener(this::previousStep);
+		previousButton.setEnabled(false);
 		buttonsPanel.add(previousButton);
+		
+		nextButton = new JButton("Next");
+		nextButton.addActionListener(this::nextStep);
+		buttonsPanel.add(nextButton);
 		
 		cancelButton = new JButton("Cancel");
 		buttonsPanel.add(cancelButton);
@@ -55,12 +69,26 @@ public abstract class AbstractWizardWindow extends JFrame {
 		windowLayout.add(bodyPanel,BorderLayout.CENTER);
 	}
 	
-	protected void addStep(String stepKey, JPanel stepPanel) {
+	protected void addStep(String stepKey, StepPanel stepPanel) {
 		bodyPanel.add(stepPanel, stepKey);
+		steps.put(stepKey, stepPanel);
 	}
 	
 	protected void gotoStep(String stepKey) {
+		currentStep = stepKey;
 		((CardLayout)bodyPanel.getLayout()).show(bodyPanel, stepKey);
+	}
+	
+	private void nextStep(ActionEvent event) {
+		passedSteps.push(currentStep);
+		previousButton.setEnabled(true);
+		StepPanel current = steps.get(currentStep);
+		gotoStep(current.next());
+	}
+	
+	private void previousStep(ActionEvent event) {
+		gotoStep(passedSteps.pop());
+		previousButton.setEnabled(!passedSteps.isEmpty());
 	}
 	
 	protected Image createWizardImage() {
@@ -75,5 +103,25 @@ public abstract class AbstractWizardWindow extends JFrame {
 			g.dispose();
 		}
 		return image;
+	}
+	
+	protected class StepPanel extends JPanel {
+
+		public StepPanel() {
+			super();
+		}
+
+		public String next() {
+			return null;
+		}
+
+		public StepPanel(LayoutManager arg0, boolean arg1) {
+			super(arg0, arg1);
+		}
+
+		public StepPanel(LayoutManager arg0) {
+			super(arg0);
+		}
+		
 	}
 }
--- pidpys-standalone/src/main/java/org/pidpys/standalone/ui/NewKeysWizardWindow.java
+++ pidpys-standalone/src/main/java/org/pidpys/standalone/ui/NewKeysWizardWindow.java
@@ -1,15 +1,14 @@
 package org.pidpys.standalone.ui;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
 import java.awt.Image;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -17,15 +16,19 @@ import org.pidpys.standalone.Resources;
 
 public class NewKeysWizardWindow extends AbstractWizardWindow {
 
+	private KeyInfoPanel keyInfoPanel;
 	private SelectAlgorithmPanel selectAlgorithmPanel;
 	
 	public NewKeysWizardWindow() {
 		super.setTitle("New Keys");
-		super.setSize(640, 480);
+		
+		keyInfoPanel = new KeyInfoPanel();
+		addStep("keyInfo", keyInfoPanel);
 		
 		selectAlgorithmPanel = new SelectAlgorithmPanel();
 		addStep("selectAlgorithm", selectAlgorithmPanel);
-		gotoStep("selectAlgorithm");
+		
+		gotoStep("keyInfo");
 	}
 
 	
@@ -40,7 +43,29 @@ public class NewKeysWizardWindow extends AbstractWizardWindow {
 		
 	}
 	
-	private class SelectAlgorithmPanel extends JPanel {
+	private class KeyInfoPanel extends StepPanel {
+		private JTextField keyNameField;
+		private JTextArea commentsArea;
+		
+		public KeyInfoPanel() {
+			super(new MigLayout("wrap 1", "[300!]", "150[][]10[][]"));
+			add(new JLabel("Name"));
+			keyNameField = new JTextField();
+			add(keyNameField, "width 300!");
+			add(new JLabel("Comments"));
+			commentsArea = new JTextArea();
+			JScrollPane scrollArea = new JScrollPane(commentsArea);
+			add(scrollArea, "width 300!, height 150!");
+		}
+
+		@Override
+		public String next() {
+			return "selectAlgorithm";
+		}
+		
+	}
+	
+	private class SelectAlgorithmPanel extends StepPanel {
 		private JComboBox algorithmSelection;
 		private JComboBox keyLengthSelection;
 		
