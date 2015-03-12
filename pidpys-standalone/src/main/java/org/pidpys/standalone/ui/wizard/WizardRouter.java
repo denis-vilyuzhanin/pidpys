083e3d1bf6a0ff1230329a451d96a06ea82054fe
Denys Viliuzhanin <dvilyuzhanin@gmail,com>
2015-03-12 23:25:47 +0200

    Remove previous implementation of wizard window

--- pidpys-standalone/src/main/java/org/pidpys/standalone/ui/wizard/WizardRouter.java
+++
@@ -1,6 +0,0 @@
-package org.pidpys.standalone.ui.wizard;
-
-public interface WizardRouter<T extends StepPanel> {
-
-	boolean route(AbstractWizardWindow wizard, T currentStep);
-}
--- pidpys-standalone/src/main/java/org/pidpys/standalone/view/NewKeysWizardView.java
+++ pidpys-standalone/src/main/java/org/pidpys/standalone/view/NewKeysWizardView.java
@@ -1,22 +1,12 @@
 package org.pidpys.standalone.view;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import javafx.application.Platform;
-
-import javax.swing.JOptionPane;
 import org.pidpys.model.NewKeysOptionsModel;
-
 import org.pidpys.standalone.controller.NewKeysWizardController;
 import org.pidpys.standalone.ui.javafx.wizard.WizardWindow;
 import org.pidpys.standalone.ui.javafx.wizard.newkeys.NewKeysConfirmationDialog;
 import org.pidpys.standalone.ui.javafx.wizard.newkeys.SelectNewKeysGeneratingFlowDialog;
-import org.pidpys.standalone.ui.wizard.AbstractWizardWindow;
-import org.pidpys.standalone.ui.wizard.newkeys.KeyInfoStepPanel;
-import org.pidpys.standalone.ui.wizard.newkeys.KeyPasswordStepPanel;
-import org.pidpys.standalone.ui.wizard.newkeys.KeyStorageStepPanel;
-import org.pidpys.standalone.ui.wizard.newkeys.SelectAlgorithmStepPanel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -58,53 +48,6 @@ public class NewKeysWizardView extends FxView {
         wizardWindow.onConfirmAction((w) -> {
             System.out.println("Confirmed");
         });
-    }
-
-    public void show1() {
-        AbstractWizardWindow wizardWindow = new AbstractWizardWindow();
-        wizardWindow.beginStep(new KeyInfoStepPanel(), this::keyInfoStepRouter);
-        wizardWindow.setVisible(true);
-    }
-
-    private boolean keyInfoStepRouter(AbstractWizardWindow wizard,
-            KeyInfoStepPanel step) {
-        wizard.gotoNextStep(new SelectAlgorithmStepPanel(),
-                this::selectAlgorithmStepRouter);
-        return true;
-    }
-
-    private boolean selectAlgorithmStepRouter(AbstractWizardWindow wizard,
-            SelectAlgorithmStepPanel step) {
-        wizard.gotoNextStep(new KeyPasswordStepPanel(),
-                this::keysPasswordStepRouter);
-        return true;
-    }
-
-    private boolean keysPasswordStepRouter(AbstractWizardWindow wizard,
-            KeyPasswordStepPanel step) {
-        char[] value = step.getPassword();
-        boolean result = true;
-        try {
-            if (value.length == 0) {
-                int reply = JOptionPane
-                        .showConfirmDialog(
-                                wizard,
-                                "This is unsecure. Are you sure that you want to continue?",
-                                "You don't specify password.",
-                                JOptionPane.YES_NO_OPTION);
-                result = reply == JOptionPane.YES_OPTION;
-            }
-            if (result) {
-                wizard.gotoNextStep(new KeyStorageStepPanel(), this::keyStorageStepRouter);
-            }
-            return result;
-        } finally {
-            Arrays.fill(value, (char) 0);
-        }
-    }
-
-    private boolean keyStorageStepRouter(AbstractWizardWindow wizard, KeyStorageStepPanel step) {
-        return true;
     }
 
    
